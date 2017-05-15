849daf9a2639194ace2ff7b1753a2bba66048e45
michelle jasmin coffin-taylor <845860590@kent-202-17m.local>
2017-05-16 10:39:03 +1200

    Completed Booleans, Arrays and Objects

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// Booleans
+
+var BooleanTrue;
+BooleanTrue = true;
+var BooleanFalse = false;
+BooleanFalse = true;
+
+console.log(BooleanTrue);
+if(BooleanTrue == false){
+	console.log("false");
+}
+
+var Test = true;
+if(!Test){
+	console.log("false");
+}
+
+//Arrays
+
+var ArrayName = ["Item1","Item2","Item3"];
+console.log(ArrayName);
+var Array2 = [
+	"Item1",
+	"Item2"
+]
+for (var i = 0; i ArrayName.length; i++) {
+
+};
+
+ArrayName[2] = "ChangedName";
+console.log(ArrayName);
+ArrayName[5] = "New Item";
+console.log(ArrayName);
+ArrayName.push("Push a new Item");
+console.log(ArrayName);
+
+//Objects
+
+var hair = "Brown"
+var AboutMe = {
+	hairColour : hair,
+	EyeColour : "Hazel",
+	Hobbies : ["Gym","Motocross"],
+	Female: female
+}
+
+console.log(AboutMe);
+console.log(AboutMe.hairColour);
+console.log(AboutMe.["EyeColour"]);
+console.log(AboutMe.["Gender"]);
+
+//Strings
+//Loops
+//For Loops
+//Prompt
+//Operator
+//Return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
