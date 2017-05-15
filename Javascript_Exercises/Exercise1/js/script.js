a43f2805f123e77cd73d734d0b25d57b8a3e34db
michelle jasmin coffin-taylor <845860590@kent-202-17m.local>
2017-05-16 11:45:15 +1200

    Started First Javascript Exercise (The Fortune Teller)

---
+++ Javascript_Exercises/Exercise1/js/script.js
@@ -0,0 +1,7 @@
+// The Fortune Teller Exercise
+
+// The Fortune TellerWhy pay a fortune teller when you can
+// just program your fortune yourself?•Store the following
+// into variables: number of children, partner's name, geographic
+// location, job title.•Output your fortune to the screen like
+// so: "You will be a X in Y, and married to Z with N kids."
